2c9f80b99bd40f132d8d26573c3f009db3b35c52
Kyu Takahashi <skallcandy0@gmail.com>
2022-04-03 17:52:51 +0900

    fix: 出力する文字列の内容を修正

--- main.go
+++ main.go
@@ -46,8 +46,9 @@ func main() {
 
 func exportOutpuFile(newsList *[]feed.News, start, end *time.Time) error {
 	fn := makeFileName(start, end)
+	dir := "export_log/"
 	msg := convertToMessage(newsList)
-	return ioutil.WriteFile(fn, []byte(msg), 0664)
+	return ioutil.WriteFile(dir+fn, []byte(msg), 0664)
 
 }
 
@@ -55,7 +56,7 @@ func makeFileName(start, end *time.Time) string {
 	dateRange := start.Format("20060102150405")
 	dateRange += "-"
 	dateRange += end.Format("20060102150405")
-	return fmt.Sprintf("log/%s.md", dateRange)
+	return fmt.Sprintf("%s.md", dateRange)
 }
 
 func convertToMessage(newsList *[]feed.News) (msg string) {
@@ -65,7 +66,7 @@ func convertToMessage(newsList *[]feed.News) (msg string) {
 
 	msg += fmt.Sprintf("# %s\n", "更新情報")
 	for _, news := range *newsList {
-		msg += fmt.Sprintf("## 【%s】\n", news.SiteTitle)
+		msg += fmt.Sprintf("## **%s**\n", news.SiteTitle)
 		for i, article := range news.Articles {
 			msg += fmt.Sprintf("### %d. %s\n", i+1, article.Title)
 			msg += fmt.Sprintf("   - 時刻: %s\n", article.PublishedParsed)
